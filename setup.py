34de3205bd91c1bdbb98ce58d4805a76d2b94790
Rosana <rosanacibely@gmail.com>
2022-09-02 18:31:34 -0300

    docs: updated email
    Co-authored-by: Rosana <rosana.rego@ufersa.edu.br>

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setuptools.setup(
     name='parkinson_disease_AI',
     url='https://github.com/cilab-ufersa/period_cycle_parkinson_disease_AI',
     author='CILAB',
-    author_email='cilab@ufersa.edu.br',
+    author_email='cilab.ufersa@gmail.com',
     # Needed to actually package something
     packages=setuptools.find_packages(),
     include_package_data=True,
