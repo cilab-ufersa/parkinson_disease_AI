1a800f7de911ca1b611d8435fb6c645f26b7048f
chrystiañ alexander <chrystian.patriota@gmail.com>
2022-10-24 20:03:30 -0300

    Feat: Added different splits for each model, it still needs to be adjusted

--- parkinson_disease_AI/main.py
+++ parkinson_disease_AI/main.py
@@ -1,7 +1,4 @@
-from distutils import errors
-from distutils.log import error
 import pickle
-from tkinter import E, Grid
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -9,83 +6,54 @@ import pandas as pd
 from imblearn.over_sampling import SMOTE
 from mlxtend.plotting import plot_learning_curves
 from sklearn.model_selection import KFold, GridSearchCV, train_test_split
-from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import StandardScaler
-from sklearn.tree import DecisionTreeClassifier
 from sklearn.svm import SVC
 
 if __name__ == "__main__":
 
-    dataset = pd.read_csv('parkinson_disease_AI\dataset\listPerson.csv')
+    dataset = pd.read_csv('..\parkinson_disease_AI\dataset\listPerson.csv')
     listDiagnosis = dataset["Diagnosis"].to_numpy()
     listPerson = dataset[["velocityWeighted", "pressureWeighted", "CISP"]]
 
     # Balance dataset
-    sm = SMOTE(random_state=13)  # Isso pode estar causando um overfitting no modelo
-    # verificar com detalhe como o método radomoversample trabalha
+    sm = SMOTE(random_state=13)
     sm.fit(listPerson, listDiagnosis)
     listPerson, listDiagnosis = sm.fit_resample(listPerson, listDiagnosis)
 
-    n_splits = 10
-    kf = KFold(n_splits=n_splits, shuffle=True, random_state=13)
+    # n_splits = 10
+    n_splits_knn = 10
+    n_splits_dt = 10
+    n_splits_rf = 10
+    n_splits_svm = 10
+
+    # kf = KFold(n_splits=n_splits, shuffle=True, random_state=13)
+    kf_knn = KFold(n_splits=n_splits_knn, shuffle=True, random_state=13)
+    kf_dt = KFold(n_splits=n_splits_dt, shuffle=True, random_state=13)
+    kf_rf = KFold(n_splits=n_splits_rf, shuffle=True, random_state=13)
+    kf_svm = KFold(n_splits=n_splits_svm, shuffle=True, random_state=13)
 
     # KNN
-    '''knnc = GridSearchCV(KNeighborsClassifier(), param_grid=[
-        {
-            'weights': ['uniform', 'distance'],
-            'n_neighbors': [i for i in range(1, 20)],
-            'p': [i for i in range(1, 6)]
-        }
-    ], cv=n_splits)'''
-    '''knnc = KNeighborsClassifier(**{'n_neighbors': 17, 'p': 1, 'weights': 'distance'})
-    
-    with open('knnc.pkl', 'wb') as file:
-        pickle.dump(knnc, file)'''
-
-    with open('parkinson_disease_AI/knnc.pkl', 'rb') as file:
+    with open('..\parkinson_disease_AI/knnc.pkl', 'rb') as file:
         knnc = pickle.load(file)
 
     # Decision Tree
-    '''cartc = GridSearchCV(estimator=DecisionTreeClassifier(), param_grid={'criterion': ['gini', 'entropy', 'log_loss'],
-                                                                         'splitter': ["best", "random"],
-                                                                         'max_depth': np.arange(10, 30, 2)
-                                                                         }, cv=n_splits)'''
-    #cartc = DecisionTreeClassifier(**{'criterion': 'entropy', 'max_depth': 20, 'splitter': 'random'})
-
-    '''with open('cartc.pkl', 'wb') as file:
-        pickle.dump(cartc, file)'''
-    
-    with open('parkinson_disease_AI/cartc.pkl', 'rb') as file:
+    with open('..\parkinson_disease_AI/cartc.pkl', 'rb') as file:
         cartc = pickle.load(file)
 
     # Random Forest
-    """rfc = GridSearchCV(estimator=RandomForestClassifier(), param_grid={'n_estimators': np.arange(6, 16, 2), 
-    'criterion': ['gini', 'entropy', 'log_loss'], 'max_features': ['sqrt', 'log2'], 'bootstrap': [True, False], 
-    'max_depth': np.arange(10, 30, 5)}, cv=n_splits) """
-
-    # {'bootstrap': True, 'criterion': 'log_loss', 'max_depth': 23, 'max_features': 'sqrt', 'n_estimators': 10}
-    """rfc = RandomForestClassifier(
-        **{'bootstrap': True, 'criterion': 'log_loss', 'max_depth': 23, 'max_features': 'sqrt', 'n_estimators': 10})
-
-    with open('rfc_model.pkl', 'wb') as file:
-        pickle.dump(rfc, file)"""
-
-    with open('parkinson_disease_AI/rfc_model.pkl', 'rb') as file:
+    with open('..\parkinson_disease_AI/rfc_model.pkl', 'rb') as file:
         rfc = pickle.load(file)
 
-
     # SVM
-    #clf = SVC(kernel='linear')
-    clf = GridSearchCV(estimator=SVC(), param_grid={'kernel': ['linear', 'poly', 'rbf', 'sigmoid'], 'gamma': ['auto', 'scale']}, cv=n_splits)
+    # clf = SVC(kernel='linear')
+    clf = GridSearchCV(estimator=SVC(),
+                       param_grid={'kernel': ['linear', 'poly', 'rbf', 'sigmoid'], 'gamma': ['auto', 'scale']},
+                       cv=n_splits_svm)
 
     ss = StandardScaler()
     TP_TN_FP_FN = np.zeros((4, 4))
 
-    diagnosis = []
-    guesseddiagnosisknn = []
-    guesseddiagnosisdt = []
-    guesseddiagnosisrf = []
-
+    '''
     for train_index, test_index in kf.split(listPerson, y=listDiagnosis):
 
         x_train, x_test = listPerson.iloc[train_index], listPerson.iloc[test_index]
@@ -99,7 +67,7 @@ if __name__ == "__main__":
         knnc_trained = knnc.fit(x_train, y_train)
         cartc_trained = cartc.fit(x_train, y_train)
         rfc_trained = rfc.fit(x_train, y_train)
-        clf_trained = clf.fit(x_train, y_train) 
+        clf_trained = clf.fit(x_train, y_train)
 
         modelsc = [knnc_trained, cartc_trained, rfc_trained, clf_trained]
 
@@ -109,12 +77,6 @@ if __name__ == "__main__":
         diagnosis.append(y_test[:])
         for model in modelsc:
             predict = model.predict(x_test)
-            if model == modelsc[0]:
-                guesseddiagnosisknn.append(model.predict(x_test))
-            elif model == modelsc[1]:
-                guesseddiagnosisdt.append(model.predict(x_test))
-            else:
-                guesseddiagnosisrf.append(model.predict(x_test))
 
             for i in range(y_test.shape[0]):
                 if y_test[i] == 0 and predict[i] == 0:
@@ -126,9 +88,110 @@ if __name__ == "__main__":
                 elif y_test[i] == 0 and predict[i] == 1:
                     TP_TN_FP_FN[j][2] += 1
             j += 1
+    '''
+
+    # KNN
+    for train_index, test_index in kf_knn.split(listPerson, y=listDiagnosis):
+
+        x_train, x_test = listPerson.iloc[train_index], listPerson.iloc[test_index]
+        y_train, y_test = listDiagnosis[train_index], listDiagnosis[test_index]
+
+        x_train = ss.fit_transform(x_train)
+        x_train = pd.DataFrame(x_train, columns=['velocityWeighted', 'pressureWeighted', 'CISP'])
+        x_test = ss.transform(x_test)
+        x_test = pd.DataFrame(x_test, columns=['velocityWeighted', 'pressureWeighted', 'CISP'])
+
+        knnc_trained = knnc.fit(x_train, y_train)
+
+        predict = knnc_trained.predict(x_test)
+
+        for i in range(y_test.shape[0]):
+            if y_test[i] == 0 and predict[i] == 0:
+                TP_TN_FP_FN[0][1] += 1
+            elif y_test[i] == 1 and predict[i] == 1:
+                TP_TN_FP_FN[0][0] += 1
+            elif y_test[i] == 1 and predict[i] == 0:
+                TP_TN_FP_FN[0][3] += 1
+            elif y_test[i] == 0 and predict[i] == 1:
+                TP_TN_FP_FN[0][2] += 1
+
+    # Decision Tree
+    for train_index, test_index in kf_dt.split(listPerson, y=listDiagnosis):
+
+        x_train, x_test = listPerson.iloc[train_index], listPerson.iloc[test_index]
+        y_train, y_test = listDiagnosis[train_index], listDiagnosis[test_index]
+
+        x_train = ss.fit_transform(x_train)
+        x_train = pd.DataFrame(x_train, columns=['velocityWeighted', 'pressureWeighted', 'CISP'])
+        x_test = ss.transform(x_test)
+        x_test = pd.DataFrame(x_test, columns=['velocityWeighted', 'pressureWeighted', 'CISP'])
+
+        cartc_trained = cartc.fit(x_train, y_train)
+
+        predict = cartc_trained.predict(x_test)
+
+        for i in range(y_test.shape[0]):
+            if y_test[i] == 0 and predict[i] == 0:
+                TP_TN_FP_FN[1][1] += 1
+            elif y_test[i] == 1 and predict[i] == 1:
+                TP_TN_FP_FN[1][0] += 1
+            elif y_test[i] == 1 and predict[i] == 0:
+                TP_TN_FP_FN[1][3] += 1
+            elif y_test[i] == 0 and predict[i] == 1:
+                TP_TN_FP_FN[1][2] += 1
+
+    # Random Forest
+    for train_index, test_index in kf_rf.split(listPerson, y=listDiagnosis):
+
+        x_train, x_test = listPerson.iloc[train_index], listPerson.iloc[test_index]
+        y_train, y_test = listDiagnosis[train_index], listDiagnosis[test_index]
+
+        x_train = ss.fit_transform(x_train)
+        x_train = pd.DataFrame(x_train, columns=['velocityWeighted', 'pressureWeighted', 'CISP'])
+        x_test = ss.transform(x_test)
+        x_test = pd.DataFrame(x_test, columns=['velocityWeighted', 'pressureWeighted', 'CISP'])
+
+        rfc_trained = rfc.fit(x_train, y_train)
+
+        predict = rfc_trained.predict(x_test)
+
+        for i in range(y_test.shape[0]):
+            if y_test[i] == 0 and predict[i] == 0:
+                TP_TN_FP_FN[2][1] += 1
+            elif y_test[i] == 1 and predict[i] == 1:
+                TP_TN_FP_FN[2][0] += 1
+            elif y_test[i] == 1 and predict[i] == 0:
+                TP_TN_FP_FN[2][3] += 1
+            elif y_test[i] == 0 and predict[i] == 1:
+                TP_TN_FP_FN[2][2] += 1
+
+    # SVM
+    for train_index, test_index in kf_rf.split(listPerson, y=listDiagnosis):
+
+        x_train, x_test = listPerson.iloc[train_index], listPerson.iloc[test_index]
+        y_train, y_test = listDiagnosis[train_index], listDiagnosis[test_index]
+
+        x_train = ss.fit_transform(x_train)
+        x_train = pd.DataFrame(x_train, columns=['velocityWeighted', 'pressureWeighted', 'CISP'])
+        x_test = ss.transform(x_test)
+        x_test = pd.DataFrame(x_test, columns=['velocityWeighted', 'pressureWeighted', 'CISP'])
+
+        clf_trained = clf.fit(x_train, y_train)
+
+        predict = clf_trained.predict(x_test)
+
+        for i in range(y_test.shape[0]):
+            if y_test[i] == 0 and predict[i] == 0:
+                TP_TN_FP_FN[3][1] += 1
+            elif y_test[i] == 1 and predict[i] == 1:
+                TP_TN_FP_FN[3][0] += 1
+            elif y_test[i] == 1 and predict[i] == 0:
+                TP_TN_FP_FN[3][3] += 1
+            elif y_test[i] == 0 and predict[i] == 1:
+                TP_TN_FP_FN[3][2] += 1
 
     # TODO: Save the models
-    
+
     # Evaluation
     acc = np.empty(4)
     sens = np.empty(4)
@@ -147,26 +210,9 @@ if __name__ == "__main__":
         print(f'{mod[i]} sua {nam[1]} é de {np.round(sens[i], 2)}%')
     for i in range(4):
         print(f'{mod[i]} sua {nam[2]} é de {np.round(esp[i], 2)}%')
-    
+
     # Plots
-    """
-    # TODO: Verify theses instructions
-    limit = int(((n_splits - 1) / n_splits) * listPerson.shape[0])
-    X_train, X_test = listPerson.iloc[:limit], listPerson.iloc[limit:]
-    y_train, y_test = listDiagnosis[:limit], listDiagnosis[limit:]
-    X_train = ss.fit_transform(X_train)
-    X_train = pd.DataFrame(X_train, columns=['velocityWeighted', 'pressureWeighted', 'CISP'])
-    X_test = ss.transform(X_test)
-    X_test = pd.DataFrame(X_test, columns=['velocityWeighted', 'pressureWeighted', 'CISP'])
-
-    plot_learning_curves(X_train, y_train, X_test, y_test, rfc)
-    plt.show()"""
-
-    # limit = int(((n_splits - 1) / n_splits) * listPerson.shape[0])
-    # X_train, X_test = listPerson.iloc[:limit], listPerson.iloc[limit:]
-    # y_train, y_test = listDiagnosis[:limit], listDiagnosis[limit:]
-
-    X_train, X_test, y_train, y_test = train_test_split(listPerson, listDiagnosis, test_size=1 / n_splits,
+    X_train, X_test, y_train, y_test = train_test_split(listPerson, listDiagnosis, test_size=1 / n_splits_knn,
                                                         random_state=13)
     X_train = ss.fit_transform(X_train)
     X_train = pd.DataFrame(X_train, columns=['velocityWeighted', 'pressureWeighted', 'CISP'])
@@ -176,11 +222,32 @@ if __name__ == "__main__":
     plot_learning_curves(X_train, y_train, X_test, y_test, knnc)
     plt.show()
 
+    X_train, X_test, y_train, y_test = train_test_split(listPerson, listDiagnosis, test_size=1 / n_splits_dt,
+                                                        random_state=13)
+    X_train = ss.fit_transform(X_train)
+    X_train = pd.DataFrame(X_train, columns=['velocityWeighted', 'pressureWeighted', 'CISP'])
+    X_test = ss.transform(X_test)
+    X_test = pd.DataFrame(X_test, columns=['velocityWeighted', 'pressureWeighted', 'CISP'])
+
     plot_learning_curves(X_train, y_train, X_test, y_test, cartc)
     plt.show()
 
+    X_train, X_test, y_train, y_test = train_test_split(listPerson, listDiagnosis, test_size=1 / n_splits_rf,
+                                                        random_state=13)
+    X_train = ss.fit_transform(X_train)
+    X_train = pd.DataFrame(X_train, columns=['velocityWeighted', 'pressureWeighted', 'CISP'])
+    X_test = ss.transform(X_test)
+    X_test = pd.DataFrame(X_test, columns=['velocityWeighted', 'pressureWeighted', 'CISP'])
+
     plot_learning_curves(X_train, y_train, X_test, y_test, rfc)
     plt.show()
+
+    X_train, X_test, y_train, y_test = train_test_split(listPerson, listDiagnosis, test_size=1 / n_splits_svm,
+                                                        random_state=13)
+    X_train = ss.fit_transform(X_train)
+    X_train = pd.DataFrame(X_train, columns=['velocityWeighted', 'pressureWeighted', 'CISP'])
+    X_test = ss.transform(X_test)
+    X_test = pd.DataFrame(X_test, columns=['velocityWeighted', 'pressureWeighted', 'CISP'])
 
     plot_learning_curves(X_train, y_train, X_test, y_test, clf)
     plt.show()
